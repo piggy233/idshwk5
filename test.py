73ca3b92c337ea9d87f9a456670187378b0fe9e0
piggy233 <62420299+piggy233@users.noreply.github.com>
2020-05-05 18:41:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 domainlist = []
 class Domain:
-  def __init__(self,_name,_label,_min,_max,_numip,_ipset);
-  self.name = _name
-  self.label = _label
-  self.ttlmin = _min
-  self.ttlmax = _max
-  self.numip = _numip
-  self.ipset = _ipset
+  def __init__(self,_name,_label,_min,_max,_numip,_ipset):
+    self.name = _name
+    self.label = _label
+    self.ttlmin = _min
+    self.ttlmax = _max
+    self.numip = _numip
+    self.ipset = _ipset
   
   def returnData(self):
     return [self.ttlmin,self.ttlmax,self.numip]
@@ -20,4 +20,14 @@ class Domain:
       return 1
     
 def initData(filename):
+  with open(filename) as f:
+    for line in f:
+      line = line.strip()
+      if line.startswith("#") or line=="":
+        continue
+      tokens = line.split(",")
+      name = tokens[0]
+      label = tokens[1]
+      ttlmin = int(tokens[2])
+        
   
