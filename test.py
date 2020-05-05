ee9720afc5dae9b7dc1ea0dff11ba66ac19623a5
piggy233 <62420299+piggy233@users.noreply.github.com>
2020-05-05 13:22:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-hhhhh
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+domainlist = []
+class Domain:
+  def __init__(self,_name,_label,_min,_max,_numip,_ipset);
+  self.name = _name
+  self.label = _label
+  self.ttlmin = _min
+  self.ttlmax = _max
+  self.numip = _numip
+  self.ipset = _ipset
+  
+  def returnData(self):
+    return [self.ttlmin,self.ttlmax,self.numip]
+  
+  def returnLabel(self):
+    if self.label == "good":
+      return 0
+    else:
+      return 1
+    
